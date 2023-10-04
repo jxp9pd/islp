78f3d29681c5d87daa0ec2f79221022f33e21609
johnpentakalos <jxp9pd@virginia.edu>
2023-10-04 09:46:01 -0400

    Added some files for OpenEMPI stuff

--- open_empi/embed_record.py
+++ open_empi/embed_record.py
@@ -1,16 +1,21 @@
 from sentence_transformers import SentenceTransformer
 import ray
 
+ray.init()
+
 HF_MODEL = SentenceTransformer('all-mpnet-base-v2')
+HF_MODEL_REF = ray.put(HF_MODEL)
 
 @ray.remote
-def remote_embed_record(model, record):
+def remote_embed_record(record):
     """
     Returns a vector embedding for a single string
 
     record:parameter concatenated string
     embedding:return list of embedding for given concat string
     """
+    model = ray.get(HF_MODEL_REF)
+    return model.encode(record)
 
-    embedding = model.encode(record)
-    return embedding
+def shutdown_ray():
+    ray.shutdown()
